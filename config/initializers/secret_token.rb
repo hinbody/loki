6feefd0c082e3c53250df5e49d77b2bd12300337
Harley Inbody <hinbody@gmail.com>
2013-01-09 18:18:49 -0500

    Initial commit, add rspec-rails and capybara to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loki2::Application.config.secret_token = 'c065beadeba681897d909af6a81187be2f1875f3355f667741f0e8fe5c1cbc5b112ed770b7e789814bd904310b423dddd417bbf973d93171c96ca9e8476d6699'
